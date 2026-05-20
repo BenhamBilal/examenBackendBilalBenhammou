27cc871ed9afe24015546a7bf8e992ffc3c8ba65
benbilal94 <benhambilal94@gmail.com>
2026-05-20 21:32:51 +0200

    route voor admincontroller aanmaken

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,7 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [App\Http\Controllers\Userzone\ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::middleware(['auth', 'admin'])->prefix('admin')->name('admin.')->group(function (){
+    Route::resource('users', App\Http\Controllers\Admin\UserController::class);
+});
 require __DIR__.'/auth.php';
